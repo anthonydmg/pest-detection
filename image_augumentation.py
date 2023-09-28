deacae7b72e0a64069a93ca85e4c8a22d998aaca
anthony <anthonymirandagil@gmail.com>
2023-09-28 16:46:24 -0500

    []

--- image_augumentation.py
+++ image_augumentation.py
@@ -8,6 +8,9 @@ import os
 import glob
 import math
 import pandas as pd
+from tqdm import tqdm
+
+tqdm.pandas()
 
 BOX_COLOR = (255, 0, 0) # Red
 TEXT_COLOR = (255, 255, 255) # White
@@ -42,14 +45,46 @@ def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
     )
     return img
 
-def visualize(image, bboxes, category_ids, category_id_to_name):
+def visualize_orig_aug_image(aug_image_df):
+    total_images = len(aug_image_df) + 1
+    n_colums = 4
+    n_rows =  math.ceil(total_images/2) if total_images > 2 else 1 
+    fig, axes = plt.subplots(nrows= n_rows, ncols= n_colums, figsize = (12,10))
+    for i in range(n_rows):
+        for j in range(n_colums):
+            if i * n_colums + j >= total_images:
+                continue
+                  
+            if i == 0 and j ==0:
+                row = aug_image_df.iloc[0]
+                image_path = row["image_path"]
+                label_path = row["label_path"]
+            else:
+                    row = aug_image_df.iloc[i * n_colums + j - 1]
+                    image_path = row["image_output_path"]
+                    label_path = row["label_output_path"] 
+            
+            image = cv2.imread(image_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+           
+            bboxes, labels = read_labels(label_path)
+            visualize(image, bboxes, labels, category_id_to_name, axis = axes[i,j])
+    
+    plt.tight_layout()
+    plt.show()
+
+
+def visualize(image, bboxes, category_ids, category_id_to_name, axis = None):
     img = image.copy()
     for bbox, category_id in zip(bboxes, category_ids):
         class_name = category_id_to_name[category_id]
         img = visualize_bbox(img, bbox, class_name)
-    plt.figure(figsize=(12, 8))
-    plt.axis('off')
-    plt.imshow(img)
+    if axis is None:
+        plt.figure(figsize=(12, 8))
+        plt.axis('off')
+        plt.imshow(img)
+    else:
+        axis.imshow(img)
 
 def yolo2albumentations(bbox):
     xmin, ymin = bbox[0]-bbox[2]/2, bbox[1]-bbox[3]/2
@@ -60,8 +95,16 @@ def yolo2albumentations(bbox):
 
     return [xmin, ymin, xmax, ymax]
 
-def read_labels(label_path, image_name):
-    with open(os.path.join(label_path, image_name+'.txt'), 'r') as f:
+def albumentations2yolo(bbox):
+    xmin, ymin, xmax, ymax = bbox
+    xc = (xmin + xmax) / 2
+    yc = (ymin + ymax) / 2
+    w = xmax - ymin
+    h = ymax - ymin
+    return [xc,yc,w, h]
+
+def read_labels(label_path):
+    with open(label_path) as f:
         bboxes = []
         labels = []
         label_lines = f.readlines()
@@ -71,8 +114,7 @@ def read_labels(label_path, image_name):
             y_c = min(float(y_c),1.0) 
             w = min(round(float(w),4),1.0)
             h = min(round(float(h),4),1.0)
-            bbox = yolo2albumentations([x_c, y_c, w, h])
-            bboxes.append(bbox)
+            bboxes.append([x_c, y_c, w, h])
             labels.append(label)
     return bboxes, labels
 
@@ -207,12 +249,6 @@ def save_labels(output_path_label, bboxes, category_ids):
 
 from tqdm.auto import tqdm
 
-#transform = A.Compose(
-#    A.HorizontalFlip(p=0.5),
-#    A.ShiftScaleRotate(p=0.5),
-#    A.RandomBrightnessContrast(p=0.3),
-#    bbox_params=A.BboxParams(format='albumentations', label_fields=['category_ids']),
-#)
 
 def create_imagedf(data_path):
     image_df = pd.DataFrame()
@@ -223,52 +259,61 @@ def create_imagedf(data_path):
     all_images.extend(glob.glob(images_path+'/*.jpeg'))
     image_df["image_path"] = all_images
     image_df["image_root"] = image_df["image_path"].apply(os.path.dirname)
-    image_df["label_path"] = image_df["image_path"].apply(lambda x: x.replace(".jpg",".txt"))
+    image_df["label_path"] = image_df["image_path"].apply(lambda x: x.replace("images","labels").replace(".jpg",".txt"))
     ## Radom images
     output_path = f"{os.path.dirname(data_path)}/aug"
-    print("output_path:", output_path)
     os.makedirs(f"{output_path}/images", exist_ok= True)
     os.makedirs(f"{output_path}/labels", exist_ok= True)
-    #image_df["image_output_path"] = image_df.image_path.str.replace(images_path, f"{output_path}/images", regex = False)
-    #image_df["image_output_path"] = image_df.image_path.str.replace(images_path, f"{output_path}/images", regex = False)
 
-    image_aug_df = image_df.sample(frac=3, replace=True, random_state=1).copy()
-    #image_aug_df.drop(['index'], axis=1, inplace=True)
+    image_aug_df = image_df.sample(frac=2, replace=True, random_state=1).copy()
     image_aug_df.reset_index(inplace=True)
     image_aug_df['IMAGE_ID'] = image_aug_df.index
-    image_df["image_output_path"] = image_aug_df['IMAGE_ID'].apply()
-    
-    print(image_aug_df.head(20))
+    image_aug_df["image_output_path"] = image_aug_df['IMAGE_ID'].apply(lambda image_id: f"{output_path}/images/AUG_{image_id:06d}.jpg")
+    image_aug_df["label_output_path"] = image_aug_df['IMAGE_ID'].apply(lambda image_id: f"{output_path}/labels/AUG_{image_id:06d}.txt")
+    print("Total de imagenes a aumentar:", len(image_aug_df))
+    return image_aug_df
 
 def aug_image(row, transform):
     image_path = row["image_path"]
+    label_path = row["label_path"]
+    image_output_path = row["image_output_path"]
+    label_output_path = row["label_output_path"]
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #image_name = '.'.join(image_path.split(os.path.sep)[-1].split('.')[:-1])
+    #labels_path = f"{os.path.dirname(image_path)}/labels"
+    bboxes, labels = read_labels(label_path)
+    bboxes = [ yolo2albumentations(bbox) for bbox in bboxes]
+    transformed = transform(image=image, bboxes=bboxes, category_ids=labels)
+    image = transformed['image']
+    bboxes = transformed['bboxes']
+    labels = transformed['category_ids']
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite(image_output_path, image)
+    bboxes = [ albumentations2yolo(bbox) for bbox in bboxes]
+    save_labels(label_output_path, bboxes, labels)
 
 
-def augmentation_imagesv1(data_path, transform):
-    images_path = f"{data_path}/images"
-    all_images = []
-    all_images.extend(glob.glob(images_path+'/*.jpg'))
-    all_images.extend(glob.glob(images_path+'/*.JPG'))
-    output_path = f"{os.path.dirname(data_path)}/aug"
-    print("output_path:", output_path)
-    os.makedirs(f"{output_path}/images", exist_ok= True)
-    os.makedirs(f"{output_path}/labels", exist_ok= True)
-    for id_img ,image_path in tqdm(enumerate(all_images)):
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_name = '.'.join(image_path.split(os.path.sep)[-1].split('.')[:-1])
-        labels_path = f"{os.path.dirname(images_path)}/labels"
-        bboxes, labels = read_labels(labels_path, image_name)
-        print("bboxes:", bboxes)
-        transformed = transform(image=image, bboxes=bboxes, category_ids=labels)
-        save_transformed(transformed, output_path, image_name, image_id = id_img, prefix = "AUG")
-        break
-
-def augmentation_images():
+def augmentation_images(data_path, transform):
     image_aug_df = create_imagedf(data_path)
+    image_aug_df.apply(lambda row: aug_image(row, transform), axis= 1)
+    original_image_path = random.choice(image_aug_df["image_path"].unique().tolist())
+    visualize_orig_aug_image(image_aug_df[image_aug_df["image_path"] == original_image_path].copy())
+    
 
-data_path = "datasets/lyromiza/data"
+data_path = "./datasets/lyromiza/data"
 
-create_imagedf(data_path)
+transform = A.Compose(
+    [A.HorizontalFlip(p=0.5),
+     A.VerticalFlip(p=0.5),
+     A.RandomBrightnessContrast(p=0.5),
+     A.GaussNoise(p=0.5),
+     A.MotionBlur(p=0.5)],
+    bbox_params=A.BboxParams(format='albumentations', label_fields=['category_ids']),
+)
 
-#augmentation_images(data_path, transform)
+augmentation_images(data_path, transform)
+#create_imagedf(data_path)
+
+#augmentation_images(data_path, transform)  
